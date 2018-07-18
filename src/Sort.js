2bc937a055253628a886387be32c7bacbf80f718
Kimiko Motoyama <meowlingual@gmail.com>
2018-07-19 00:08:18 +0900

    deleted unnecessary comments

--- src/Sort.js
+++ src/Sort.js
@@ -6,8 +6,6 @@ class Sort {
   sort() {
     let resultArray = [];
 
-    // resultArray = [2]
-    // this.array = [3, 1, 5]
     let element = this.array.pop();
     resultArray.push(element);
     const length = this.array.length;
@@ -16,15 +14,13 @@ class Sort {
     while (i < length) {
       if (resultArray.length === length + 1) return resultArray;
       if (this.array.length > 0) {
-        element = this.array.pop(); //element = 1,
+        element = this.array.pop();
       }
       if (element < resultArray[i]) {
-        // 1 < 2? no.
         resultArray.splice(i, 0, element);
         i = 0;
       } else {
         if (i + 1 === resultArray.length) {
-          //if there's no more element in resultArray
           resultArray.push(element);
           i = 0;
         } else {
